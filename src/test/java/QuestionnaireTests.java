c309a088b4b3ccc8a67c077d0bc3be224870397f
Rzesiasty <andrzej.splewinski@gmail.com>
2023-05-05 14:12:49 +0200

    Second commit

--- src/test/java/QuestionnaireTests.java
+++ src/test/java/QuestionnaireTests.java
@@ -55,7 +55,5 @@ public class QuestionnaireTests {
         WebElement sport = driver.findElement(By.className("moj-select")).findElement(By.cssSelector("option[value='koszykowka']"));
         sport.click();
 
-
     }
-
 }
